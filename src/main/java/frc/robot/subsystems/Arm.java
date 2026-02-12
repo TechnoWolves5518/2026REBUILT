06c3a4a7449742c9617e531348f3bf8fdf6651f4
OrangeAed <ajsimonsrudolph@gmail.com>
2026-02-12 18:39:32 -0500

    basic start

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -4,14 +4,26 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
+  private double angle; // Current height of the arm in meters
+  private PIDController pidController; // PID controller for arm height control
   /** Creates a new Arm. */
-  public Arm() {}
+  public Arm(PIDController pidController) {
+    this.pidController = pidController;
+    angle = 0.0; // Initialize the arm angle to 0 degrees
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void setAngle(double targetAngle) {
+    // Convert target angle to height using the arm's geometry
+    double targetHeight = Math.sin(Math.toRadians(targetAngle)) * 1.0; // Assuming arm length is 1 meter
+    
   }
 }
